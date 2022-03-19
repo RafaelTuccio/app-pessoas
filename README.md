7c611d19707693fd451fdfea0b731ce9ae7e0110
RafaelTuccio <rafael_tuccio@hotmail.com>
2022-03-19 20:45:37 -0300

    :feat: criado o component Navbar e criado o historico de desenvolvimento no README

---
+++ README.md
@@ -0,0 +1,7 @@
+# APP USUARIOS
+
+## Histórico de criação 
+
+### - Criar o projeto com create-react-app
+### - Adicionar o axios ao projeto
+### - Criar o Component Navbar
---
+++ src/Component/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+const Navbar = (props) => {
+    return(
+        <nav className="navbar">
+            <h1 className="logo">Logo</h1>
+            <ul class="ul-navbar">  
+                <li className="li-navbar">Link 1</li>
+                <li className="li-navbar">Link 2</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
